f53ef997e8e203063607bbc8869eafde0f7ea4e0
Maria <mari.dvoyeryadkina@list.ru>
2020-09-03 22:40:55 +0900

    com

--- app/src/main/java/com/example/recycleractivity/Adapter.java
+++ app/src/main/java/com/example/recycleractivity/Adapter.java
@@ -3,6 +3,7 @@ package com.example.recycleractivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,7 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolderInfo> {
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
     private List<User> users;
 
@@ -22,48 +23,53 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolderInfo> {
         this.users = users;
     }
 
-
-    // создание ресайкла
     @NonNull
     @Override
-    public ViewHolderInfo onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        // создание вьюхолдера
-        return new ViewHolderInfo(
-                // переводим макет
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false)
-        );
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View userView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list, parent, false);
+        ViewHolder viewHolder = new ViewHolder(userView);
+        return viewHolder;
     }
 
-    // наполнение данными
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderInfo holder, int position) {
-        holder.bind(users.get(position));
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+        User user = users.get(position);
+        TextView textView = holder.itemView.findViewById(R.id.textViewInfo);
+        textView.setText(user.getName());
+
+        Button button = holder.itemView.findViewById(R.id.buttonRec);
+        button.setText(user.isInfo() ? "Детали" : "Добавить");
     }
 
-    // сколько всего элементов в списке
     @Override
     public int getItemCount() {
         if (users == null) {
             return 0;
-        }
-        return users.size();
+        } return users.size();
     }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 
     // создание холдера
-    static class ViewHolderInfo extends RecyclerView.ViewHolder {
+//    static class ViewHolderInfo extends RecyclerView.ViewHolder {
+//
+//        //для получения текствью
+//        private final TextView textView;
+//
+//        // конструктор
+//        public ViewHolderInfo(@NonNull View itemView) {
+//            super(itemView);
+//            textView = itemView.findViewById(R.id.textViewInfo);
+//        }
 
-        //для получения текствью
-        private final TextView textView;
-
-        // конструктор
-        public ViewHolderInfo(@NonNull View itemView) {
-            super(itemView);
-            textView = itemView.findViewById(R.id.textViewInfo);
-        }
-
-        // метод наполнения информацией
-        void bind(User users) {
-            textView.setText(users);
-        }
-    }
+//        // метод наполнения информацией
+//        void bind(User user) {
+//            textView.setText(user);
+//        }
+//    }
 }
